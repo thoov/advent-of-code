175af865c13a376e4532ab126228aa8aebc7e856
Travis Hoover <thoov7@gmail.com>
2021-12-02 11:12:42 -0800

    day 2

---
+++ src/day2/mod.rs
@@ -0,0 +1,86 @@
+use std::fs;
+
+pub fn solve() -> (isize, isize) {
+    let contents =
+        fs::read_to_string("src/day2/input.txt").expect("Something went wrong reading the file");
+
+    (part_1(&contents), part_2(&contents))
+}
+
+fn part_1(contents: &str) -> isize {
+    let mut horizontal_pos = 0;
+    let mut depth = 0;
+
+    for command_str in contents.lines() {
+        let command_split: Vec<&str> = command_str.split_whitespace().collect();
+        let command = command_split[0];
+        let amount: isize = command_split[1].parse().unwrap();
+
+        match command {
+            "forward" => horizontal_pos += amount,
+            "down" => depth += amount,
+            "up" => depth -= amount,
+            _ => println!("Unknown command"),
+        }
+    }
+
+    horizontal_pos * depth
+}
+
+fn part_2(contents: &str) -> isize {
+    let mut horizontal_pos = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for command_str in contents.lines() {
+        let command_split: Vec<&str> = command_str.split_whitespace().collect();
+        let command = command_split[0];
+        let amount: isize = command_split[1].parse().unwrap();
+
+        match command {
+            "forward" => {
+                horizontal_pos += amount;
+                depth += aim * amount;
+            },
+            "down" => aim += amount,
+            "up" => aim -= amount,
+            _ => println!("Unknown command"),
+        }
+    }
+
+    horizontal_pos * depth
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve_works() {
+        assert_eq!(solve(), (1728414, 1765720035));
+    }
+
+    #[test]
+    fn part_1_works() {
+        let test_str = String::from("forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2");
+
+        assert_eq!(part_1(&test_str), 150);
+    }
+
+    #[test]
+    fn part_2_works() {
+        let test_str = String::from("forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2");
+
+        assert_eq!(part_2(&test_str), 900);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day1;
+mod day2;
 
 fn main() {
     let (part_1, part_2) = day1::solve();
+    println!("Day 1 --- Part 1: {} Part 2: {}", part_1, part_2);
+
+    let (part_1, part_2) = day2::solve();
     println!("Day 1 --- Part 1: {} Part 2: {}", part_1, part_2);
 }
